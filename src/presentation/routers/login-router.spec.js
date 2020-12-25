c98f096eba45204f212515b59ac4b4c3e30a43f8
Pedro Alexandre <alexdevcoder@gmail.com>
2020-12-25 15:51:04 -0300

    refactor: [factory] add helper to build the SUT instance

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,10 +1,16 @@
 const LoginRouter = require('./login-router')
 const MissingParamError = require('../helpers/missing-param-error')
 
+// Design pattern: Factory
+// Avoid crashing other places that call the object
+const buildSUT = () => {
+  return new LoginRouter()
+}
+
 describe('Login Router', () => {
   test('Should return 400 if email is not provided', () => {
     // sut = system under test
-    const sut = new LoginRouter()
+    const sut = buildSUT()
     const httpRequest = {
       body: {
         password: 'foo_password'
@@ -17,7 +23,7 @@ describe('Login Router', () => {
 
   test('Should return 400 if password is not provided', () => {
     // sut = system under test
-    const sut = new LoginRouter()
+    const sut = buildSUT()
     const httpRequest = {
       body: {
         email: 'foo_email@email.com'
@@ -30,14 +36,14 @@ describe('Login Router', () => {
 
   test('Should return 500 if no httpRequest is provided', () => {
     // sut = system under test
-    const sut = new LoginRouter()
+    const sut = buildSUT()
     const httpResponse = sut.route()
     expect(httpResponse.statusCode).toBe(500)
   })
 
   test('Should return 500 if httpRequest has no body', () => {
     // sut = system under test
-    const sut = new LoginRouter()
+    const sut = buildSUT()
     const httpRequest = {}
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
